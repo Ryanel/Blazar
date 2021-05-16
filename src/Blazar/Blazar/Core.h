764525b7cde21b13c4e8c6087183df804a8eabd0
Corwin McKnight <corwin.mcknight@gmail.com>
2021-05-16 13:35:41 -0700

    Core: Change Assertion and Verification Language

--- src/Blazar/Blazar/Core.h
+++ src/Blazar/Blazar/Core.h
@@ -3,6 +3,7 @@
 #define BIT(x) (1 << x)
 
 #ifdef BLAZAR_PLATFORM_WINDOWS
+
 #ifdef BLAZAR_BUILD_STATIC
 #define BLAZAR_API
 #endif
@@ -14,6 +15,7 @@
 #define BLAZAR_API __declspec(dllimport)
 #endif
 #endif
+
 #else
 #error Blazar only supports windows.
 #endif
@@ -34,26 +36,27 @@
         }                                                         \
     }
 
-#define BLAZAR_VERIFY(x, ...)                                        \
-    {                                                                \
-        if (!(x)) {                                                  \
-            LOG_GAME_ERROR("Verification Failed: {0}", __VA_ARGS__); \
-            __debugbreak();                                          \
-        }                                                            \
+#define BLAZAR_VERIFY(x, ...)                                             \
+    {                                                                     \
+        if (!(x)) {                                                       \
+            LOG_GAME_ERROR("Runtime Assertion Failed: {0}", __VA_ARGS__); \
+            __debugbreak();                                               \
+        }                                                                 \
     }
-#define BLAZAR_CORE_VERIFY(x, ...)                                  \
-    {                                                                \
-        if (!(x)) {                                                  \
-            LOG_CORE_ERROR("Verification Failed: {0}", __VA_ARGS__); \
-            __debugbreak();                                          \
-        }                                                            \
+#define BLAZAR_CORE_VERIFY(x, ...)                                        \
+    {                                                                     \
+        if (!(x)) {                                                       \
+            LOG_CORE_ERROR("Runtime Assertion Failed: {0}", __VA_ARGS__); \
+            __debugbreak();                                               \
+        }                                                                 \
     }
 #else
+
 #define BLAZAR_ASSERT(x, ...)
 #define BLAZAR_CORE_ASSERT(x, ...)
-
 #define BLAZAR_VERIFY(x, ...) x
 #define BLAZAR_CORE_VERIFY(x, ...) x
+
 #endif
 
 #define BLAZAR_BIND_EVENT_FN(x) std::bind(&x, this, std::placeholders::_1)
